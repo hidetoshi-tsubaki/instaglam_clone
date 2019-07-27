1c05c92a9bd49ae2d208777f5329e6200b6d3ab0
hidetoshitsubaki <hidetoshi.tsubaki@gmail.com>
2019-07-27 18:35:40 +0900

    add jscroll to comment

--- app/assets/javascripts/infinite_scroll.js
+++ app/assets/javascripts/infinite_scroll.js
@@ -1,5 +1,5 @@
 $(function () {
-  $('.jscroll').jscroll({
+  $('.jscroll,.comment_jscroll').jscroll({
     contentSelector: '.scroll_list',
     nextSelector: 'span.next:last a'
   });
@@ -9,6 +9,17 @@ $(window).on('scroll', function () {
   scrollPosition = $(window).height() + $(window).scrollTop();
   if ((scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
     $('.jscroll').jscroll({
+      contentSelector: '.skill-list',
+      nextSelector: 'span.next:last a'
+    });
+  }
+});
+$(window).on('scroll', function () {
+  scrollHeight = $("#comments").get(0).scrollHeight;
+  commentHeight = $("#comments").get(0).offsetHeight;
+  scrollPosition = commentHeight + $(".comments").scrollTop();
+  if ((scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
+    $('.comment_jscroll').jscroll({
       contentSelector: '.skill-list',
       nextSelector: 'span.next:last a'
     });
